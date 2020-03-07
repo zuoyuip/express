f45f6d7731451ad30b20b8da2d4797e4b0dc90a6
zuoyu <zuoyuip@qq.com>
2020-03-07 18:10:03 +0800

    项目初始化

---
+++ express-server/src/main/java/org/fast/express/ExpressApplication.java
@@ -0,0 +1,16 @@
+package org.fast.express;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zuoyu
+ */
+@SpringBootApplication
+public class ExpressApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ExpressApplication.class, args);
+  }
+
+}
---
+++ express-server/src/test/java/org/fast/express/ExpressApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fast.express;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+abstract class ExpressApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
